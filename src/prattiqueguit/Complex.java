98774820e0a51817aaf33fc49c545ca0b8b7020e
Wilfied <elmagnificoe4@gmail.com>
2022-03-11 14:27:22 +0100

    Creation de la classe complexe

---
+++ src/prattiqueguit/Complex.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prattiqueguit;
+
+/**
+ *
+ * @author HP
+ */
+public class Complex {
+    private double reel;
+    private double imaginaire;
+
+    public Complex() {
+    }
+
+    public Complex(double reel, double imaginaire) {
+        this.reel = reel;
+        this.imaginaire = imaginaire;
+    }
+
+    public double getImaginaire() {
+        return imaginaire;
+    }
+
+    public void setReel(double reel) {
+        this.reel = reel;
+    }
+
+    public void setImaginaire(double imaginaire) {
+        this.imaginaire = imaginaire;
+    }
+
+    public double getReel() {
+        return reel;
+    }
+
+    @Override
+    public String toString() {
+        return "Complex{" + "reel=" + reel + ", imaginaire=" + imaginaire + '}';
+    }
+
+    
+}
